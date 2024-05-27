a7158757959927b41ff4e36329e40d068132a509
Copiumツ <103134141+PotatoDonkey749@users.noreply.github.com>
2024-05-27 12:51:43 +0200

    Update main.py

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,9 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
-
-print(input)
+import re
+with open("./input.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        word = re.sub(r'[^a-zA-Z0-9]', '', line.strip().lower())
+        reverse = word[::-1]
+        is_palindrome = word == reverse
+        unique = ''.join(set(word))
+        print(f'{"YES" if is_palindrome else "NO"}, {"-1" if not is_palindrome else len(unique)}')
